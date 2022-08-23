547db112ebb6f7d22a278b8e30fd936f0f086045
aaron <kaven22314567@gmail.com>
2022-08-24 00:30:18 +0800

    [feat] add header & style

--- src/App.js
+++ src/App.js
@@ -1,5 +1,15 @@
+import React from 'react';
+import { Routes, Route, Link } from 'react-router-dom';
+import Header from './components/public_component/Header';
+import './styles/style.css';
+
 function App() {
-  return <></>;
+  return (
+    <>
+      <Header />
+      <Routes>{/* <Route path="/" element={<Homepage />} /> */}</Routes>
+    </>
+  );
 }
 
 export default App;
