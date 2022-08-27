240fb473a4daf6c237f61308377d2a2b00817e69
Mike <s33165309000@gmail.com>
2022-08-27 13:41:14 +0800

    main pull

--- src/components/product/product-list/ProductRank.js
+++ src/components/product/product-list/ProductRank.js
@@ -1,7 +1,16 @@
 import React from 'react';
 
 const ProductRank = () => {
-  return <div>ProductRank</div>;
+  return (
+    <>
+      <div className="d-flex align-items-center justify-content-between ">
+        <h4>商品一覽</h4>
+        <p>
+          LIFE {'>'} 商品 {'>'} 所有家電 {'>'} 所有分類
+        </p>
+      </div>
+    </>
+  );
 };
 
 export default ProductRank;
