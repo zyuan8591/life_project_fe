5f92a988cbdf97088b5732d04665b964bf81fa4f
aaron <kaven22314567@gmail.com>
2022-08-27 12:09:24 +0800

    [feat] update index tile component

--- src/components/index/Homepage.js
+++ src/components/index/Homepage.js
@@ -1,12 +1,34 @@
 import React from 'react';
 import SliderComponent from './component/SliderComponent';
 import '../../styles/_homepage.scss';
+import IndexTitle from './component/IndexTitle';
+import IndexNews from './component/IndexNews';
 
 const Homepage = () => {
   return (
     <>
       <SliderComponent />
-      <div className="homepage"></div>
+      <div className="homepageContainer">
+        <div className="homepage">
+          <IndexTitle title="最新資訊" subtitle="News" route="/news" />
+          <IndexNews />
+          <IndexTitle
+            title="最新商品"
+            subtitle="New Products"
+            route="/products"
+          />
+          <IndexTitle
+            title="熱門食譜"
+            subtitle="Popular Recipes"
+            route="/recipes"
+          />
+          <IndexTitle
+            title="活動專區"
+            subtitle="Activities"
+            route="/activity"
+          />
+        </div>
+      </div>
     </>
   );
 };
