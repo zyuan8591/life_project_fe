21efd4e521991e74a2938ee5f99416924227ff1d
Quan <cat814051@gmail.com>
2022-08-30 15:31:18 +0800

    test

--- src/components/Login/login/login.js
+++ src/components/Login/login/login.js
@@ -10,13 +10,20 @@ const login = () => {
             <label className="" htmlFor="">
               註冊信箱：
             </label>
-            <input className="mt-3" type="text" />
+            <input className="mt-3 inputStyle" type="text" />
           </div>
           <div className="mt-5">
             <label htmlFor="">密碼：</label>
-            <input className='mt-3' type="password" />
+            <input className="mt-3 inputStyle" type="password" />
+          </div>
+          <div className="d-flex mt-2">
+            <input type="radio" />
+            <label className="ms-2 fs-6">記住帳號密碼</label>
           </div>
           <button className="mt-5">登入</button>
+
+          {/* TODO:
+           1.從cookie取得使用者帳密 */}
         </form>
         <div className="row mt-2">
           <div className="col ">
