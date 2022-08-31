500af7810583706f56ffaafc4e3b088406398b6e
Sandy <a0919613087@gmail.com>
2022-08-31 19:31:53 +0800

    [feat] add ActivityBanner.js

---
+++ src/components/camping/camping_main/component/ActivityBanner.js
@@ -0,0 +1,25 @@
+import React from 'react';
+
+function ActivityBanner() {
+  return (
+    <>
+      <div className="banner">
+        <img
+          src="/img/camping/activity_camping_img/main_img_8.jpg"
+          alt="camping"
+        />
+      </div>
+
+      {/* <p className="intText">
+            一個人也想露營嗎？ <br />
+            一起加入露營活動認識更多的朋友
+            <br />
+            主打懶人露營，讓您如同入住酒店般舒適
+            <br />
+            再也不用一身狼狽同樣能享受野趣的全新體驗
+          </p> */}
+    </>
+  );
+}
+
+export default ActivityBanner;
