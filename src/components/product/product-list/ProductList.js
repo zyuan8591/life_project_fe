2d2e506176d54fae139c5d3a694f6758dbd12cf0
Mike <s33165309000@gmail.com>
2022-08-31 15:28:06 +0800

    [feat] add productFilter add product scss folder

--- src/components/product/product-list/ProductList.js
+++ src/components/product/product-list/ProductList.js
@@ -3,17 +3,23 @@ import ProductRank from './ProductRank';
 import ProductCategory from './ProductCategory';
 import ProductFilter from './ProductFilter';
 // import productClass from '../../../styles/productList.module.scss';
-import '../../../styles/_productList.scss';
+import '../../../styles/product/_productList.scss';
+import Header from '../../public_component/Header';
+import Footer from '../../public_component/Footer';
 
 const ProductList = () => {
   return (
-    <div className="product">
-      <ProductRank />
-      <div className="d-flex mt-5 ">
-        <ProductCategory />
-        <ProductFilter />
+    <>
+      <Header />
+      <div className="product">
+        <ProductRank />
+        <div className="d-flex mt-5 ">
+          <ProductCategory />
+          <ProductFilter />
+        </div>
       </div>
-    </div>
+      <Footer />
+    </>
   );
 };
 
