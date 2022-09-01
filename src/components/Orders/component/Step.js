5eb4956c6b71b8d7ab723406fe71a22a95ce8858
Allen <rcvr1024@gmail.com>
2022-09-01 16:07:04 +0800

    [feat] add stepnavigation component

--- src/components/Orders/component/Step.js
+++ src/components/Orders/component/Step.js
@@ -1,22 +1,40 @@
 import React from 'react';
+import { IconContext } from 'react-icons';
+import { FiShoppingBag } from 'react-icons/fi';
+import { AiOutlineEdit, AiOutlineCheck } from 'react-icons/ai';
 
-export default function Step() {
+const Step = (props) => {
+  const stepIcon = (p) => {
+    switch (p) {
+      case 1:
+        return <FiShoppingBag />;
+      case 2:
+        return <AiOutlineEdit />;
+      case 3:
+        return <AiOutlineCheck />;
+      default:
+        break;
+    }
+  };
+
   return (
     <>
-      <div className="wizard">
-        <div className="wizard__step">
-          <div className="wizard__dot">
-            <div className="wizard__number">1</div>
-            <div className="wizard__connector"></div>
-
-            <div className="wizard__number">2</div>
-            <div className="wizard__connector"></div>
-
-            <div className="wizard__number">3</div>
+      <div className={'stepBlock ' + (props.selected ? 'selected' : '')}>
+        <div
+          className={'circleWrapper'}
+          onClick={() => props.updateStep(props.index + 1)}
+        >
+          <div className="circle">
+            {' '}
+            <IconContext.Provider value={{ color: '#444', size: '2rem' }}>
+              {stepIcon(props.index + 1)}
+            </IconContext.Provider>
           </div>
-          123
         </div>
+        <span>{props.label}</span>
       </div>
     </>
   );
-}
+};
+
+export default Step;
