4f22032341acfb5bf4713ad0649cc4ed75af827f
Sandy <a0919613087@gmail.com>
2022-09-03 12:37:06 +0800

    [feat] add slider

--- src/components/camping/camping_main/component/ActivitySliderHeadcount.js
+++ src/components/camping/camping_main/component/ActivitySliderHeadcount.js
@@ -1,11 +1,24 @@
 import React from 'react';
+import { Slider } from 'antd';
+import 'antd/dist/antd.css';
+
+const onAfterChange = (value) => {
+  console.log('onAfterChange: ', value);
+};
 
 function ActivitySliderHeadcount() {
   return (
     <>
       <div className="activitySlider">
         <p className="sliderText">活動人數</p>
-        <input type="range" className="slider" />
+        <Slider
+          className="slider"
+          range
+          step={1}
+          defaultValue={[0, 100]}
+          // onChange={onChange}
+          onAfterChange={onAfterChange}
+        />
         <div className="d-flex sliderSearch">
           <div>3人 - 12人</div>
           <button>篩選</button>
--- src/components/camping/camping_main/component/ActivitySliderPrice.js
+++ src/components/camping/camping_main/component/ActivitySliderPrice.js
@@ -1,11 +1,24 @@
 import React from 'react';
+import { Slider } from 'antd';
+import 'antd/dist/antd.css';
+
+const onAfterChange = (value) => {
+  console.log('onAfterChange: ', value);
+};
 
 function ActivitySliderPrice() {
   return (
     <>
       <div className="activitySlider">
         <p className="sliderText">活動費用</p>
-        <input type="range" className="slider" />
+        <Slider
+          className="slider"
+          range
+          step={1}
+          defaultValue={[0, 100]}
+          // onChange={onChange}
+          onAfterChange={onAfterChange}
+        />
         <div className="d-flex sliderSearch">
           <div>$100 - $3000</div>
           <button>篩選</button>
