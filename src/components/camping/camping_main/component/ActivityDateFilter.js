6715341adbf3d300d379fe75b18bbc33400e701f
Sandy <a0919613087@gmail.com>
2022-09-03 12:36:04 +0800

    [feat] add input style

--- src/components/camping/camping_main/component/ActivityDateFilter.js
+++ src/components/camping/camping_main/component/ActivityDateFilter.js
@@ -3,13 +3,14 @@ import React from 'react';
 function ActivityDateFilter() {
   return (
     <>
-      <div className="activityDate">
+      <div className="activityDate text-end">
         <p className="dateText">活動日期</p>
         <div className="d-flex ms-3">
-          <input type="date" />
+          <input type="date" style={{ maxWidth: '115px' }} />
           <div className="mx-2">-</div>
-          <input type="date" />
+          <input type="date" style={{ maxWidth: '115px' }} />
         </div>
+        <button>篩選</button>
       </div>
     </>
   );
