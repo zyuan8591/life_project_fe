6ac8c6ff2a356b55aea9734df631d9d0edb31852
Kelsyyy <clyn8877@gmail.com>
2022-09-05 10:22:03 +0800

    [style] update scss

--- src/App.js
+++ src/App.js
@@ -3,10 +3,13 @@ import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import Homepage from './components/index/Homepage';
 import './styles/style.scss';
-import PicnicIndex from './components/picnic/picnic_main/PicnicIndex';
-import PicnicOffical from './components/picnic/picnic_official/PicnicOffical';
 import Users from './components/Users';
 import Login from './components/Login/loginPage';
+import PicnicIndex from './components/picnic/picnic_main/PicnicIndex';
+import PicnicOffical from './components/picnic/picnic_official/PicnicOffical';
+import PicnicOfficalDetail from './components/picnic/picnic_official/picnic_offical_detail/IndexOfficalDetail';
+import IndexPrivateDetail from './components/picnic/private_pincnic/private_pincnic_detail/IndexPrivateDetail';
+import CreatePincnic from './components/picnic/private_pincnic/CreatePincnic';
 
 
 function App() {
@@ -16,6 +19,9 @@ function App() {
         <Route path="/" element={<Homepage />} />
         <Route path="/activity/picnic" element={<PicnicIndex />} />
         <Route path="/activity/picnic/official" element={<PicnicOffical />} />
+        <Route path="/activity/picnic/official/:id" element={<PicnicOfficalDetail />} />
+        <Route path="/activity/picnic/group/:id" element={<IndexPrivateDetail />} />
+        <Route path="/activity/picnic/create" element={<CreatePincnic />} />
         <Route path="/Users/*" element={<Users />} />
         <Route path="/login" element={<Login />} />
       </Routes>
