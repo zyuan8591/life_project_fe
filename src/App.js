fb00566fd62aa6e2686bb37bbcfd0fa3d4288ce7
aaron <kaven22314567@gmail.com>
2022-09-06 14:49:53 +0800

    [feat] add scroll to top

--- src/App.js
+++ src/App.js
@@ -21,6 +21,7 @@ import Pinic from './components/Users/Content/Picnic/Pinic';
 import Recipe from './components/Users/Content/Recipe/Recipe';
 import Signup from './components/Login/Signup';
 import Login from './components/Login/login';
+import ScrollToTop from './components/public_component/ScrollToTop';
 
 function HeaderFooter() {
   return (
@@ -36,29 +37,31 @@ function HeaderFooter() {
 function App() {
   return (
     <>
-      <Routes>
-        <Route path="/" element={<HeaderFooter />}>
-          <Route path="/" element={<Homepage />} />
-          <Route path="/recipes" element={<Recipes />} />
-        </Route>
-        <Route path="/recipes/:recipeId" element={<RecipeDetail />} />
-        <Route path="/products" element={<ProductList />} />
-        <Route path="/products/:id" element={<ProductDetail />} />
-        <Route path="/news" element={<News />} />
-        <Route path="/users/" element={<Users />}>
-          <Route path="/users/account" element={<Account />} />
-          <Route path="/users/password" element={<Password />} />
-          <Route path="/users/order" element={<Order />} />
-          <Route path="/users/points" element={<Points />} />
-          <Route path="/users/pinic" element={<Pinic />} />
-          <Route path="/users/recipe" element={<Recipe />} />
-        </Route>
-        <Route path="/login/" element={<LoginPage />}>
-          <Route path="/login/" element=<Login /> />
-          <Route path="/login/signup" element={<Signup />} />
-        </Route>
-        {/* <Route path="*" element={<NotFound />} /> */}
-      </Routes>
+      <ScrollToTop>
+        <Routes>
+          <Route path="/" element={<HeaderFooter />}>
+            <Route path="/" element={<Homepage />} />
+            <Route path="/recipes" element={<Recipes />} />
+          </Route>
+          <Route path="/recipes/:recipeId" element={<RecipeDetail />} />
+          <Route path="/products" element={<ProductList />} />
+          <Route path="/products/:id" element={<ProductDetail />} />
+          <Route path="/news" element={<News />} />
+          <Route path="/users/" element={<Users />}>
+            <Route path="/users/account" element={<Account />} />
+            <Route path="/users/password" element={<Password />} />
+            <Route path="/users/order" element={<Order />} />
+            <Route path="/users/points" element={<Points />} />
+            <Route path="/users/pinic" element={<Pinic />} />
+            <Route path="/users/recipe" element={<Recipe />} />
+          </Route>
+          <Route path="/login/" element={<LoginPage />}>
+            <Route path="/login/" element=<Login /> />
+            <Route path="/login/signup" element={<Signup />} />
+          </Route>
+          {/* <Route path="*" element={<NotFound />} /> */}
+        </Routes>
+      </ScrollToTop>
     </>
   );
 }
