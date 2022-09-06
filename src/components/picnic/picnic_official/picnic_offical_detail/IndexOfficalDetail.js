df888d26e5317d50e72c2eb37fcc72a5e2f23bf1
Kelsyyy <clyn8877@gmail.com>
2022-09-06 10:40:46 +0800

    [erro] silder scss 無法顯示

--- src/components/picnic/picnic_official/picnic_offical_detail/IndexOfficalDetail.js
+++ src/components/picnic/picnic_official/picnic_offical_detail/IndexOfficalDetail.js
@@ -1,15 +1,16 @@
 import React from 'react';
+import { useState } from 'react';
 import '../../../../styles/picnic/_picnicOfficalDetail.scss';
 import Header from '../../../public_component/Header';
 import Footer from '../../../public_component/Footer';
 import BackToTop from '../../../public_component/BackToTop';
-import DetailTitle from '../../picnic_cpmpoent/DetailTitle';
+import DetailTitle from '../../picnic_compoent/DetailTitle';
 import OffcialDetailContent from './OffcialDetailContent';
-import RecommendProducts from '../../picnic_cpmpoent/RecommendProducts';
-import Paicipant from '../../picnic_cpmpoent/Paicipant';
+import RecommendProducts from '../../picnic_compoent/RecommendProducts';
+import Paicipant from '../../picnic_compoent/Paicipant';
 import PaicipantInfo from './PaicipantInfo';
-import AsideMessageFix from '../../picnic_cpmpoent/AsideMessageFix';
-import RecommendActivity from '../../picnic_cpmpoent/RecommendActivity';
+import AsideMessageFix from '../../picnic_compoent/AsideMessageFix';
+import RecommendActivity from '../../picnic_compoent/RecommendActivity';
 import {
   FaPaw,
   FaExclamationTriangle,
@@ -22,25 +23,43 @@ import { BsPersonFill } from 'react-icons/bs';
 import { IoIosArrowBack, IoIosArrowForward } from 'react-icons/io';
 
 function PicnicOfficalDetail() {
+  const [scrollDown, setScrollDown] = useState(false);
+
+  let scrollY = window.scrollY;
+  window.addEventListener('scroll', () => {
+    let scrollNow = window.scrollY;
+    setScrollDown(scrollNow > scrollY);
+    scrollY = scrollNow;
+  });
+
   return (
     <>
       <Header />
       <div>LIFE / 活動專區 / 野餐專區 / 官方活動 / 夏季野餐趣</div>
-      <main className="PicnicOfficalDetailContainer container row">
-        <div className="mainWrap col-sm-8">
-          {/* 上方活動資訊和圖片 */}
-          <DetailTitle />
-          {/* 活動詳細內容 */}
-          <OffcialDetailContent />
-          {/* 參加者 */}
-          <Paicipant />
-          <RecommendProducts />
+      <main className="PicnicOfficalDetailContainer container ">
+        <div className="main row">
+          <div className="mainWrap col-sm-8">
+            {/* 上方活動資訊和圖片 */}
+            <DetailTitle />
+            {/* 活動詳細內容 */}
+            <OffcialDetailContent />
+            {/* 參加者 */}
+            <Paicipant />
+            {/* 推薦商品 */}
+            <RecommendProducts />
+          </div>
+          <div className="col-sm-3">
+            {/* 側邊資訊欄 */}
+            <AsideMessageFix
+              scrollDown={scrollDown}
+              setScrollDown={setScrollDown}
+            />
+          </div>
+          {/* 熱門活動 */}
+          <RecommendActivity />
         </div>
-        <RecommendActivity />
-        {/* 側邊資訊欄 */}
-        <AsideMessageFix />
       </main>
-      <PaicipantInfo />
+      {/* <PaicipantInfo /> */}
       <Footer />
       <BackToTop />
     </>
