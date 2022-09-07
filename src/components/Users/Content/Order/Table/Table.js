cabe562aed090d824626a773781c49f8793aa9b2
Quan <cat814051@gmail.com>
2022-09-07 15:39:39 +0800

    123

--- src/components/Users/Content/Order/Table/Table.js
+++ src/components/Users/Content/Order/Table/Table.js
@@ -31,30 +31,32 @@ const Table = () => {
 
   return (
     <div className="order-table">
-      <table className="table table-bordered mt-5">
-        {title.map((v, i) => {
-          return (
-            <thead key={i}>
-              <th>{v}</th>
-            </thead>
-          );
-        })}
+      <table className="table table-sm mt-5">
+        <thead>
+          <tr>
+            {title.map((v, i) => {
+              return <th key={i}>{v}</th>;
+            })}
+          </tr>
+        </thead>
 
-        {data.map((v, i) => {
-          return (
-            <tbody key={i}>
-              <td>{v.orderNumber}</td>
-              <td>${v.total}</td>
-              <td>{v.orderState}</td>
-              <td>{v.date}</td>
-              <td>{v.payment}</td>
-              <td>{v.method}</td>
-              <td>
-                <button>訂單詳情</button>
-              </td>
-            </tbody>
-          );
-        })}
+        <tbody>
+          {data.map((v, i) => {
+            return (
+              <tr key={i}>
+                <td>{v.orderNumber}</td>
+                <td>${v.total}</td>
+                <td>{v.orderState}</td>
+                <td>{v.date}</td>
+                <td>{v.payment}</td>
+                <td>{v.method}</td>
+                <td>
+                  <button>訂單詳情</button>
+                </td>
+              </tr>
+            );
+          })}
+        </tbody>
       </table>
       {/* <div className="icons">
         <p>目前沒有訂單</p>
