f7a0a13cf2884834623c6ccb10254d586c10121d
aaron <kaven22314567@gmail.com>
2022-09-08 16:14:51 +0800

    [feat] update header

--- src/components/public_component/Header.js
+++ src/components/public_component/Header.js
@@ -3,7 +3,11 @@ import { Link, NavLink } from 'react-router-dom';
 import { v4 as uuidv4 } from 'uuid';
 import '../../styles/_header.scss';
 import { IconContext } from 'react-icons';
-import { AiOutlineSearch, AiOutlineShoppingCart } from 'react-icons/ai';
+import {
+  AiOutlineSearch,
+  AiOutlineShoppingCart,
+  AiOutlineUser,
+} from 'react-icons/ai';
 import axios from 'axios';
 import { API_URL } from '../../utils/config';
 import { useUserRights } from '../../usecontext/UserRights';
@@ -21,7 +25,6 @@ const pages = [
 const Header = ({ fixed = true }) => {
   const [search, setSearch] = useState(false);
   const [scrollDown, setScrollDown] = useState(false);
-  const [isLogin, setIsLogin] = useState(true);
   const [userSelectActive, setUserSelectActive] = useState(false);
 
   const userAvatorClickHandler = () => {
@@ -41,7 +44,7 @@ const Header = ({ fixed = true }) => {
     await axios.get(`${API_URL}/logout`, { withCredentials: true });
     setUser(null);
   }
-  console.log(user); //TODO:確認登入狀態
+  console.log(user);
   return (
     <header
       className={`header position-relative w-100 ${
@@ -90,45 +93,56 @@ const Header = ({ fixed = true }) => {
                 <AiOutlineShoppingCart />
               </Link>
             </li>
-            {/* User */}
-            {/* <li className="me-3 userItem">
-              <Link to="/users/account" className="flexCenter">
-                <AiOutlineUser />
-              </Link>
-            </li>
+
             {/* Login state display User avator */}
 
             {user ? (
-              <>
-                <li className="userItem position-relative headerLoginState">
-                  <figure
-                    className="headerAvator m-0 flexCenter cursorPointer"
-                    onClick={() => userAvatorClickHandler()}
-                  >
-                    <img
-                      src="/img/user/user_img/fish.png"
-                      alt="userAvatar"
-                      className="objectContain"
-                    />
-                  </figure>
-                  {userSelectActive && (
-                    <ul className="headerUserControl position-absolute top-100 end-0 mt-3 d-flex flex-column align-items-center fs-6 px-0 py-1">
-                      <li className="py-1">
-                        <Link to="/users/account">個人檔案</Link>
-                      </li>
-                      <li className="py-1" onClick={handelLogout}>
-                        登出
-                      </li>
-                    </ul>
-                  )}
-                </li>
-              </>
+              // IS LOGIN
+              <li className="userItem position-relative headerLoginState">
+                <figure
+                  className="headerAvator m-0 flexCenter cursorPointer"
+                  onClick={() => userAvatorClickHandler()}
+                >
+                  <img
+                    src="/img/user/user_img/fish.png"
+                    alt="userAvatar"
+                    className="objectContain"
+                  />
+                </figure>
+                {userSelectActive && (
+                  <ul className="headerUserControl position-absolute top-100 end-0 mt-3 d-flex flex-column align-items-center fs-6 px-0 py-1">
+                    <li className="py-1">
+                      <Link to="/users/account">個人檔案</Link>
+                    </li>
+                    <li className="py-1" onClick={handelLogout}>
+                      登出
+                    </li>
+                  </ul>
+                )}
+              </li>
             ) : (
-              <>
-                <Link to="/login">
-                  <button>登入</button>
-                </Link>
-              </>
+              // NOT LOGIN YET
+              <li className="userItem position-relative headerLoginState cursorPointer">
+                <div
+                  className="flexCenter loginAvator"
+                  onClick={() => userAvatorClickHandler()}
+                >
+                  <AiOutlineUser />
+                </div>
+                {userSelectActive && (
+                  <ul className="headerUserControl position-absolute top-100 end-0 mt-3 d-flex flex-column align-items-center fs-6 px-0 py-1">
+                    <li className="py-1">
+                      <Link to="/login">登入</Link>
+                    </li>
+                    <li className="py-1" onClick={handelLogout}>
+                      <Link to="/login/signup">註冊</Link>
+                    </li>
+                  </ul>
+                )}
+              </li>
+              // <Link to="/login">
+              //   <button>登入</button>
+              // </Link>
             )}
           </ul>
         </div>
