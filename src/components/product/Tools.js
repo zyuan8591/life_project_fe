f5e7236056a63b5caf8f2bce6b2fb7c4d2c0939e
Mike <s33165309000@gmail.com>
2022-09-12 23:21:51 +0800

    [feat] add Tools

--- src/components/product/Tools.js
+++ src/components/product/Tools.js
@@ -6,15 +6,20 @@ import { HiOutlineHeart } from 'react-icons/hi';
 import { IoCartOutline } from 'react-icons/io5';
 import { IoIosArrowUp } from 'react-icons/io';
 import { GoGift } from 'react-icons/go';
+import { FaTrashAlt } from 'react-icons/fa';
+const img = '/img/product/product_img/BRUNO_BOE021_BGY_01.jpeg';
 
 const Tools = () => {
-  const [cart, setCart] = useState(false);
-  const Exit = (e) => {
-    console.log(e.keyCode);
-  };
+  const [cart, setCart] = useState(true);
   return (
     <>
-      <div className={classess.tool}>
+      <div
+        className={classess.tool}
+        style={{
+          transform: cart ? 'translateX(-300px)' : 'translateX(0px)',
+          opacity: cart ? '1' : '',
+        }}
+      >
         <IconContext.Provider value={{ size: '2rem' }}>
           <HiOutlineHeart />
           <IoCartOutline
@@ -30,7 +35,7 @@ const Tools = () => {
         style={{ display: cart ? 'block' : '' }}
         className={classess.cartContainer}
         onClick={() => {
-          //   setCart(false);
+          setCart(false);
         }}
         onKeyDown={(e) => {
           if (e.keyCode === 27) {
@@ -43,12 +48,32 @@ const Tools = () => {
         style={{ transform: cart ? 'translateX(0px)' : 'translateX(300px)' }}
         className={classess.cart}
         onKeyDown={(e) => {
-          //   if (e.keyCode === 27) {
-          //     setCart(false);
-          //   }
-          Exit();
+          if (e.keyCode === 27) {
+            setCart(false);
+          }
         }}
-      ></div>
+      >
+        <div className={classess.cartTitle}>購物車</div>
+        {[...Array(3)].map((v, i) => {
+          return (
+            <div className={classess.cartItem} key={i}>
+              <figure>
+                <img src={img} alt="" />
+              </figure>
+              <div className={classess.cartInfo}>
+                <p>BOE021 多功能電烤盤-經典款</p>
+                <p>粉綠色</p>
+                <p>1 x NT$4960</p>
+              </div>
+              <div className={classess.trash} onClick={() => {}}>
+                <IconContext.Provider value={{ color: '#777', size: '1.2rem' }}>
+                  <FaTrashAlt />
+                </IconContext.Provider>
+              </div>
+            </div>
+          );
+        })}
+      </div>
     </>
   );
 };
