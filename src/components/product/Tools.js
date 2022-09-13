002878ac7b18daa320438d71a05839b1af2a7b3e
NhOuOiB <s33165309000@gmail.com>
2022-09-13 15:36:32 +0800

    [feat] tools ui complete

--- src/components/product/Tools.js
+++ src/components/product/Tools.js
@@ -2,15 +2,62 @@ import React from 'react';
 import { useState } from 'react';
 import classess from '../../styles/product/tools.module.scss';
 import { IconContext } from 'react-icons';
-import { HiOutlineHeart } from 'react-icons/hi';
+import { HiOutlineHeart, HiHeart } from 'react-icons/hi';
 import { IoCartOutline } from 'react-icons/io5';
 import { IoIosArrowUp } from 'react-icons/io';
-import { GoGift } from 'react-icons/go';
+import { AiOutlineCreditCard } from 'react-icons/ai';
 import { FaTrashAlt } from 'react-icons/fa';
 const img = '/img/product/product_img/BRUNO_BOE021_BGY_01.jpeg';
+const img1 = '/img/product/product_img/BRUNO_BOE021_01.jpeg';
+const img2 = '/img/product/product_img/BRUNO_BOE021_WH_01.webp';
 
 const Tools = () => {
-  const [cart, setCart] = useState(true);
+  const [cart, setCart] = useState(false);
+  const arr = [
+    {
+      id: 0,
+      name: 'BOE021 多功能電烤盤-經典款',
+      color: '粉綠色',
+      count: 2,
+      price: 4690,
+      img: img,
+    },
+    {
+      id: 1,
+      name: 'BOE021 多功能電烤盤-經典款',
+      color: '粉色',
+      count: 1,
+      price: 4690,
+      img: img1,
+    },
+    {
+      id: 2,
+      name: 'BOE021 多功能電烤盤-經典款',
+      color: '白色',
+      count: 7,
+      price: 4690,
+      img: img2,
+    },
+    {
+      id: 3,
+      name: 'BOE021 多功能電烤盤-經典款',
+      color: '白色',
+      count: 7,
+      price: 4690,
+      img: img2,
+    },
+    {
+      id: 4,
+      name: 'BOE021 多功能電烤盤-經典款',
+      color: '白色',
+      count: 7,
+      price: 4690,
+      img: img2,
+    },
+  ];
+  const [item, setItem] = useState(arr);
+  const [point, setPoint] = useState(false);
+  const [heart, setHeart] = useState(false);
   return (
     <>
       <div
@@ -21,13 +68,24 @@ const Tools = () => {
         }}
       >
         <IconContext.Provider value={{ size: '2rem' }}>
-          <HiOutlineHeart />
+          <HiOutlineHeart
+            onClick={() => {
+              setHeart(!heart);
+            }}
+          />
           <IoCartOutline
             onClick={() => {
               setCart(true);
             }}
           />
-          <GoGift />
+          <AiOutlineCreditCard
+            onMouseOver={() => {
+              setPoint(true);
+            }}
+            onMouseOut={() => {
+              setPoint(false);
+            }}
+          />
           <IoIosArrowUp onClick={() => window.scrollTo({ top: 0 })} />
         </IconContext.Provider>
       </div>
@@ -54,18 +112,29 @@ const Tools = () => {
         }}
       >
         <div className={classess.cartTitle}>購物車</div>
-        {[...Array(3)].map((v, i) => {
+        {item.map((v, i) => {
+          const { id, name, color, count, price, img } = v;
           return (
             <div className={classess.cartItem} key={i}>
               <figure>
                 <img src={img} alt="" />
               </figure>
               <div className={classess.cartInfo}>
-                <p>BOE021 多功能電烤盤-經典款</p>
-                <p>粉綠色</p>
-                <p>1 x NT$4960</p>
+                <p>{name}</p>
+                <p>{color}</p>
+                <p>
+                  {count} x NT${price}
+                </p>
               </div>
-              <div className={classess.trash} onClick={() => {}}>
+              <div
+                className={classess.trash}
+                onClick={(e) => {
+                  let newArr = item.filter((v2, i2) => {
+                    return id !== v2.id;
+                  });
+                  setItem(newArr);
+                }}
+              >
                 <IconContext.Provider value={{ color: '#777', size: '1.2rem' }}>
                   <FaTrashAlt />
                 </IconContext.Provider>
@@ -74,6 +143,73 @@ const Tools = () => {
           );
         })}
       </div>
+      {point && (
+        <div
+          className={classess.point}
+          style={{
+            transform: cart ? 'translateX(-300px)' : 'translateX(0px)',
+          }}
+        >
+          <p>點數 : 27 </p>
+          {/* <p> 一點折一元</p>
+          <p>每200元能獲得一點</p> */}
+          <div className={classess.triangle}></div>
+        </div>
+      )}
+      {heart && (
+        <div className={classess.favContainer}>
+          <div
+            className={classess.fav}
+            style={{
+              transform: cart ? 'translateX(-300px)' : 'translateX(0px)',
+            }}
+          >
+            {item.map((v, i) => {
+              const { id, name, color, img } = v;
+              console.log(item);
+              return (
+                <div
+                  className={classess.item}
+                  onClick={(e) => {
+                    let newArr = item.filter((v2, i2) => {
+                      return id !== v2.id;
+                    });
+                    setItem(newArr);
+                  }}
+                >
+                  {item ? (
+                    <>
+                      <figure>
+                        <img src={img} alt="" />
+                        <div className={classess.heart}>
+                          <IconContext.Provider
+                            value={{ color: 'white', size: '2.4rem' }}
+                          >
+                            <HiHeart />
+                          </IconContext.Provider>
+                        </div>
+                      </figure>
+                      <p>
+                        {name}　({color})
+                      </p>
+                    </>
+                  ) : (
+                    <p>沒有商品在我的最愛</p>
+                  )}
+                </div>
+              );
+            })}
+          </div>
+          <div
+            className={classess.triangle}
+            style={{
+              transform: cart
+                ? 'translateX(-300px) rotate(45deg)'
+                : 'translateX(0px) rotate(45deg)',
+            }}
+          ></div>
+        </div>
+      )}
     </>
   );
 };
