0a27f5d21b3e259879a7d9910020383375ec886d
NhOuOiB <s33165309000@gmail.com>
2022-09-20 12:03:41 +0800

    [feat] update recommend

--- src/components/product/product_detail/ProductListSlider.js
+++ src/components/product/product_detail/ProductListSlider.js
@@ -2,39 +2,59 @@ import axios from 'axios';
 import React from 'react';
 import { useState, useEffect } from 'react';
 import { API_URL } from '../../../utils/config';
+import { Link } from 'react-router-dom';
 
-const img = '/img/product/product_img/BRUNO_BOE059_BGR_CE_01.png';
 
 const ProductListSlider = ({ now, total, data }) => {
-  // const []
+  const [recommendArr, setRecommendArr] = useState([]);
   const { category } = data;
   console.log(category);
   useEffect(() => {
     (async () => {
-      let recommend = await axios.get(
-        `${API_URL}/products/${category}/recommend`
+      let result = await axios.get(
+        `${API_URL}/products/recommend?category=${category}`
       );
-      console.log(recommend);
+      console.log(result.data);
+      setRecommendArr(result.data);
     })();
   }, [category]);
+
   return (
     <>
-      {[...Array(total)].map((v, i) => {
+      {recommendArr.map((v, i) => {
+        const { id, name, img, color, price } = v;
         return (
           <>
-            <figure
-              style={{
-                transform: `translateX(${now}px)`,
-              }}
-              key={i}
-            >
-              <img src={img} alt="" />
-              <p>
-                <span className="me-2">BRUNO</span>多功能電烤盤-經典款
-                <span className="ms-2">(白色)</span>
-              </p>
-              <p>NT$ 3,290</p>
-            </figure>
+            <Link to={`/products/${id}`} >
+              <figure
+                style={{
+                  transform: `translateX(${now}px)`,
+                }}
+                key={i}
+              >
+                <div
+                  style={{
+                    minHeight: '345px',
+                    display: 'flex',
+                    alignItems: 'center',
+                  }}
+                >
+                  <img src={`/img/product/product_img/${img}`} alt="" />
+                </div>
+                <p>
+                  <span className="me-2">BRUNO</span>
+                  {name}
+                  <span className="ms-2">({color})</span>
+                </p>
+                <p>
+                  NT${' '}
+                  {JSON.stringify(price).replace(
+                    /\B(?<!\.\d*)(?=(\d{3})+(?!\d))/g,
+                    ','
+                  )}
+                </p>
+              </figure>
+            </Link>
           </>
         );
       })}
