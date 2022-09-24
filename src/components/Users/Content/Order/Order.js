48bf41baac4b4eab66bd4af5adb599a50a1c670b
Quan <cat814051@gmail.com>
2022-09-24 13:38:11 +0800

    feat

--- src/components/Users/Content/Order/Order.js
+++ src/components/Users/Content/Order/Order.js
@@ -11,45 +11,29 @@ const Order = () => {
   const [list, setList] = useState('全部');
   const [pageNow, setPageNow] = useState(1);
   const [lastPage, setLastPage] = useState(0);
-  const [data, setData] = useState([
-    {
-      orderNumber: 123456,
-      total: 800,
-      orderState: '運送中',
-      date: '2022-04-18',
-      payment: '信用卡',
-      method: '貨到付款',
-    },
-    {
-      orderNumber: 1615616,
-      total: 1600,
-      orderState: '備貨中',
-      date: '2022-05-18',
-      payment: '綠界',
-      method: '超商取貨',
-    },
-  ]);
+  const [data, setData] = useState([]);
   const [display, setDisplay] = useState(0);
-  // const getUser = async (apiurl) => {
-  //   let response = await axios.get(apiurl, { withCredentials: true });
-  //   setData(response.data.result);
-  //   setLastPage(response.data.pagination.lastPage);
-  // };
+  const getUser = async (apiurl) => {
+    let response = await axios.get(apiurl, { withCredentials: true });
+    setData(response.data.result);
+    setLastPage(response.data.pagination.lastPage);
+  };
+  console.log(data);
   // useEffect(() => {
   //   let apiurl = '';
   //   (async () => {
   //     switch (display) {
   //       case 1:
-  //         apiurl = `${API_URL}/picnic/group/memberJoin?page=${pageNow}`;
+  //         apiurl = `${API_URL}/orders/group/memberJoin?page=${pageNow}`;
   //         break;
   //       case 2:
-  //         apiurl = `${API_URL}/picnic/official/memberJoin`;
+  //         apiurl = `${API_URL}/orders/official/memberJoin`;
   //         break;
   //       case 3:
-  //         apiurl = `${API_URL}/official/memberCollect`;
+  //         apiurl = `${API_URL}/orders/memberCollect`;
   //         break;
   //       default: //0
-  //         apiurl = `${API_URL}/picnic/official/memberJoin?page=${pageNow}`;
+  //         apiurl = `${API_URL}/orders`;
   //         break;
   //     }
   //     getUser(apiurl);
