1e82d4017ec878e1d5287f98b066cbe835db20e3
aaron <kaven22314567@gmail.com>
2022-09-28 17:34:20 +0800

    [feat] update chat style

--- src/components/backstage/BackstageChat.js
+++ src/components/backstage/BackstageChat.js
@@ -4,26 +4,45 @@ import { v4 as uuidv4 } from 'uuid';
 import { API_URL, API_URL_IMG } from '../../utils/config';
 import axios from 'axios';
 import classes from '../../styles/moduleCss/backstage/backstageChat.module.scss';
+import { Input } from 'antd';
+import { IconContext } from 'react-icons';
+import { AiOutlineSend } from 'react-icons/ai';
 
 const BackstageChat = () => {
+  const { TextArea } = Input;
+  const [userNow, setUserNow] = useState(0);
   const [message, setMessage] = useState('');
   const [messages, setMessages] = useState([
     {
       id: '28d745cb-0038-4aba-9881-5fe123bf7598',
-      role: 'life',
-      content: '1:456',
+      user_id: 1,
+      content: '你好 1',
     },
     {
       id: 'f3ddea23-e89c-495d-899d-37083997a40c',
-      role: 'life',
-      content: '1:123',
+      user_id: 1,
+      content: '你好 2',
     },
     {
       id: 'bee46ffb-1635-4776-a997-209980ffc3fb',
-      role: 'life',
-      content: '1:456',
+      user_id: 3,
+      content: '你好 3',
+    },
+    {
+      id: 'bee46ffb-1635-4776-a997-209980ffc3fb',
+      user_id: 0,
+      // target_id: 5,
+      content: '你好 4',
     },
   ]);
+  const [displayMsg, setDisplayMsg] = useState([]);
+  useEffect(() => {
+    let msg = messages.filter((msg) => {
+      return msg.user_id === parseInt(userNow) || msg.user_id === 0;
+    });
+    setDisplayMsg(msg);
+  }, [userNow, messages]);
+
   // cosnt[(user, setUser)] = useState(null);
   const [socket, setSocket] = useState(null);
   const [userData, setUserData] = useState([]);
@@ -38,7 +57,7 @@ const BackstageChat = () => {
         let { id, msg } = message;
         setMessages(function (prevState, props) {
           return [
-            { id: uuidv4(), role: 'life', content: `${id}:${msg}` },
+            { id: uuidv4(), user_id: 'life', content: `${id}:${msg}` },
             ...prevState,
           ];
         });
@@ -55,23 +74,22 @@ const BackstageChat = () => {
     e.preventDefault();
     // 把訊息送到後端去
     if (message) {
-      socket.emit('user1', { id: 0, msg: message });
+      // socket.emit('user1', { id: 0, msg: message });
       setMessages([
-        { id: uuidv4(), role: 'user', content: message },
+        { id: uuidv4(), user_id: 0, content: message },
         ...messages,
       ]);
       setMessage('');
     }
   };
-  console.log(messages);
+
   return (
-    <div>
-      {/* {messages.map((msg) => (
-        <div key={msg.id}>{msg.content}</div>
-      ))} */}
-      <div className="userList">
+    <div className={classes.container}>
+      {/* chatroom list ================================================*/}
+      <section className={classes.userList}>
         {userData.map((user) => (
-          <div className={classes.userMsg}>
+          <div className={classes.userMsg} onClick={() => setUserNow(user.id)}>
+            {/* user avatar */}
             <figure className={classes.userAvatarContainer}>
               <img
                 src={`${API_URL_IMG}${user.photo}`}
@@ -79,13 +97,52 @@ const BackstageChat = () => {
                 className="objectContain"
               />
             </figure>
+            {/* userName & userMsg */}
             <div className={classes.msgInfo}>
               <div>{user.name}</div>
               <div>123456</div>
             </div>
           </div>
         ))}
-      </div>
+      </section>
+      {/* msg box section ===============================================*/}
+      <section className={classes.msgBox}>
+        {/* chatroom title */}
+        <div className={classes.title}>皮卡啾 {userNow}</div>
+        <div>
+          {/* chatroom */}
+          <ul className={`d-flex flex-column-reverse ${classes.msgList}`}>
+            {displayMsg.map((msg) => (
+              <li
+                className={`${
+                  msg.user_id === 0 ? `align-self-end` : classes.bgMsg
+                } `}
+              >
+                {msg.content}
+              </li>
+            ))}
+          </ul>
+          {/* chat msg box */}
+          <div className={classes.msgSendBox}>
+            <TextArea
+              placeholder="輸入訊息"
+              value={message}
+              autoSize={{ minRows: 1, maxRows: 3 }}
+              onChange={(e) => setMessage(e.target.value)}
+              className="w-100"
+            />
+            <IconContext.Provider value={{ color: '#444', size: '1.6rem' }}>
+              <button
+                className="flexCenter bg-transparent border-0"
+                onClick={(e) => submitMsg(e)}
+              >
+                <AiOutlineSend />
+              </button>
+            </IconContext.Provider>
+          </div>
+        </div>
+      </section>
+      {/* ))} */}
     </div>
   );
 };
